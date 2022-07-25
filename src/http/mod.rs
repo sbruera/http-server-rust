710adc0dd073820d072f2f0e5aaee29f0b637d76
sbruera <santibruera97@gmail.com>
2022-07-24 21:51:23 -0300

    Lifetime annotations

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,4 +1,6 @@
 pub use request::Request;
+pub use request::ParseError;
+
 pub use method::Method;
 
 pub mod request;
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use std::io::Read;
 use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
 
 pub struct Server {
     addr: String,
@@ -24,6 +26,13 @@ impl Server {
                     match stream.read(&mut buffer){
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                }
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e)
                     }
